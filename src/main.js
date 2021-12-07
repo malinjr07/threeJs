d614987af4338670acedf3eaa8431188d5bffeea
malinjr07 <malinjr07@gmail.com>
2021-12-07 16:35:29 +0600

    light added

--- src/main.js
+++ src/main.js
@@ -33,8 +33,8 @@ const props = {
   fullWidth: window.innerWidth,
   fullHeight: window.innerHeight,
   floorColor: '#a9c388',
-  ambient: '#ffffff',
-  ambientOpacity: 0.5,
+  ambient: '#b9b5ff',
+  ambientOpacity: 0.12,
 };
 
 /**
@@ -97,10 +97,10 @@ house.add(roof);
 
 const door = new THREE.Mesh(
   new THREE.PlaneBufferGeometry(1.5, 2),
-  new THREE.MeshBasicMaterial({ color: '#ffffff' })
+  new THREE.MeshBasicMaterial({ color: '#aa7b7b' })
 );
-door.position.z = walls.geometry.parameters.depth / 2 + 0.1;
-door.position.y = door.geometry.parameters.height / 2 + 0.01;
+door.position.z = walls.geometry.parameters.depth / 2 + 0.001;
+door.position.y = door.geometry.parameters.height / 2;
 house.add(door);
 
 // Bushes
@@ -128,7 +128,27 @@ scene.add(bush4);
 
 // Graves
 
+const graveyard = new THREE.Group();
+scene.add(graveyard);
 
+const graveGeo = new THREE.BoxBufferGeometry(0.6, 0.8, 0.2);
+const graveMaterial = new THREE.MeshStandardMaterial({ color: '#b2b6b1' });
+
+for (let i = 0; i < 50; i++) {
+  const angle = Math.random() * Math.PI * 2;
+  const radious = 4 + Math.random() * 4.5;
+  const x = Math.sin(angle) * radious;
+  const z = Math.cos(angle) * radious;
+  const grave = new THREE.Mesh(graveGeo, graveMaterial);
+  grave.position.set(
+    x,
+    grave.geometry.parameters.height / 2 - Math.random(),
+    z
+  );
+  grave.rotation.y = (Math.random() - 0.5) * 0.4;
+  grave.rotation.z = (Math.random() - 0.5) * 0.4;
+  graveyard.add(grave);
+}
 
 /**
  * Lights Init
@@ -146,10 +166,20 @@ const moonlight = new THREE.DirectionalLight('#ffffff', 0.5);
 moonlight.position.set(4, 5, -2);
 scene.add(moonlight);
 
+// Door Light
+
+const doorLight = new THREE.PointLight('#ff7d46', 1, 7);
+doorLight.position.set(
+  0,
+  walls.geometry.parameters.height,
+  walls.geometry.parameters.depth / 2 + 1
+);
+house.add(doorLight);
+
 // Debugger
 const lights = gui.addFolder('Lights');
 
-const moonDebug = lights.addFolder('Point Light');
+const doorDebug = lights.addFolder('Point Light');
 const ambientDebug = lights.addFolder('Ambient Light');
 
 ambientDebug
@@ -161,9 +191,28 @@ ambientDebug
   .onChange(() => {
     ambient.intensity = props.ambientOpacity;
   });
+doorDebug
+  .add(doorLight.position, 'x')
+  .min(0)
+  .max(5)
+  .step(0.001)
+  .name('doorLight x position');
+doorDebug
+  .add(doorLight.position, 'y')
+  .min(0)
+  .max(5)
+  .step(0.001)
+  .name('doorLight y position');
+doorDebug
+  .add(doorLight.position, 'z')
+  .min(0)
+  .max(5)
+  .step(0.001)
+  .name('doorLight z position');
 
 lights.open();
-ambientDebug.open();
+// ambientDebug.open();
+doorDebug.open();
 
 /**
  * Camera Init
